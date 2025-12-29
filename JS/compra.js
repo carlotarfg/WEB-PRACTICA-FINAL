841c30d3804517c1908aac616cd91cb0b0b99944
Teresa-cpu <teresaningzhen.garcia@alumnos.udit.es>
2025-12-29 15:38:03 +0100

    avance parte izquierda del producto

--- JS/compra.js
+++ JS/compra.js
@@ -1,16 +1,23 @@
-// ___ CLICKAR IMG Y QUE SALGAN OPCIONES ___
-
 $(document).ready(function () {
 
   $(".tab").on("click", function () {
 
     const category = $(this).data("category");
+    const $categoryDiv = $(".category." + category);
 
+    // SI LA ACTIVAS SALEN LAS OPCIONES
+    if ($categoryDiv.hasClass("active")) {
+      $categoryDiv.removeClass("active");
+      $(this).removeClass("active");
+      return;
+    }
+
+    // SI VUELVES A CLIKAR LA IMG SE OCULTA DE NUEVO 
     $(".tab").removeClass("active");
     $(".category").removeClass("active");
 
     $(this).addClass("active");
-    $(".category." + category).addClass("active");
+    $categoryDiv.addClass("active");
 
     if (category === "paypal") {
       window.location.href = "https://www.paypal.com/checkoutnow?token=65U984472L9049229";
